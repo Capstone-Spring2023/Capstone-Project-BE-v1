39c81e8e404adb44a131616fdfee58280a1dca49
Darrow Trinh <tqdatqn01230@gmail.com>
2023-04-09 10:22:14 +0700

    feature: add leader management

--- API/Controllers/Leader/LeaderManagementController.cs
+++ API/Controllers/Leader/LeaderManagementController.cs
@@ -14,9 +14,11 @@ namespace API.Controllers
     public class LeaderManagementController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly CFManagementContext _context;
 
-        public LeaderManagementController(IUserService userService)
+        public LeaderManagementController(IUserService userService, CFManagementContext context)
         {
+            _context = context;
             _userService = userService;
         }
 
@@ -45,7 +47,49 @@ namespace API.Controllers
                 return NotFound();
             }
             return Ok(response);
-        }       
+        }
+        [HttpPost]
+        [Route("api/leader")]
+        public async Task<ObjectResult> CreateLeader([FromBody] LeaderCreateRequest request)
+        {
+            var aSubject = _context.AvailableSubjects.Find(request.AvailableSubjectId);
+            aSubject.LeaderId = request.UserId;
+            var user = _context.Users.Find(request.UserId);
+            aSubject.LeaderName = user.FullName;
+            user.RoleId = 3;
+            await _context.SaveChangesAsync();
+            return new ObjectResult("Create Success")
+            {
+                StatusCode = 201,
+            };
+        }
+        [HttpPut]
+        [Route("api/leader")]
+        public async Task<ObjectResult> UpdateLeader([FromBody] LeaderCreateRequest request)
+        {
+            var aSubject = _context.AvailableSubjects.Find(request.AvailableSubjectId);
+            
+            var newLeader = _context.Users.Find(request.UserId);
+            var oldLeader = _context.Users.Find(aSubject.LeaderId);
+            aSubject.LeaderId = request.UserId;
+            if (oldLeader == null)
+            {
+                return new ObjectResult("Subject haven't have leader yet")
+                {
+                    StatusCode = 400,
+                };
+            }
+            aSubject.LeaderName = newLeader.FullName;
+
+            newLeader.RoleId = 3;
+            oldLeader.RoleId = 4;
+
+            await _context.SaveChangesAsync();
+            return new ObjectResult("Update Success")
+            {
+                StatusCode = 200,
+            };
+        }
     }
 
 
